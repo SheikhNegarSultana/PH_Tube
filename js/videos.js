15b44b84e889ed5fe7e572f8640796ca09c33803
SheikhNegarSultana <sultanashashi64@gmail.com>
2024-10-11 10:44:24 +0600

    t

--- js/videos.js
+++ js/videos.js
@@ -1,3 +1,34 @@
+function fetchCategory(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(response => response.json())
+    .then(data => displayCategory(data.categories))
+}
+
+function displayCategory(categories){
+    const categoryNav = document.getElementById('categories')
+    
+    for( const item of categories){
+        // console.log(item)
+        const buttonContainer = document.createElement('div')
+        buttonContainer.innerHTML = `
+        <button 
+        id='btn-${item.category_id}' 
+        onclick="loadCategoryVideos(${item.category_id})" 
+        class="btn btn-outline btn-primary category-btn">
+
+       ${item.category}
+
+      </button>
+        `
+        
+        categoryNav.appendChild(buttonContainer)
+
+    }
+}
+
+fetchCategory()
+
+
 function fetchVideo(){
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
     .then(response => response.json())
@@ -7,6 +38,23 @@ function fetchVideo(){
 
 function displayVideo(videos){
     const videoContainer = document.getElementById('video');
+
+    videoContainer.innerHTML='' 
+
+
+    if (videos.length == 0) {
+        videoContainer.classList.remove("grid");
+        videoContainer.innerHTML = `
+        <div class="min-h-[300px] flex flex-col gap-5 justify-center items-center">
+        
+          <img src="assets/Icon.png" /> 
+          <h2 class="text-center text-xl font-bold"> No Content Here in this Categery </h2> 
+        </div>`;
+      } else {
+        videoContainer.classList.add("grid");
+      }
+
+
     for ( const item of videos){
         console.log(item)
         const videoDiv = document.createElement('div');
@@ -20,17 +68,53 @@ function displayVideo(videos){
   </figure>
 
   <div class="card-body">
-  
     <h2 class="card-title">${item.title}</h2>
+
+    ${ item.authors[0].verified === true ? 'Verified' : 'Not Verified'}
+
     <p>${item.description}</p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary">Watch</button>
+
+      <button onclick="loadDetails('${item.video_id}')" class="btn btn-primary">Watch</button>
+
     </div>
   </div>
         `
         videoContainer.appendChild(videoDiv)
 
     }
+}
+
+function loadCategoryVideos(id){
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(response => response.json())
+    .then(data => {
+          //sobaike active class remove korao
+           removeActiveClass();
+
+         //id er class k active korao
+        const activebtn = document.getElementById(`btn-${id}`) //Button Color Change
+        activebtn.classList.add('active')
+        displayVideo(data.category)
+
+    })
+}
+
+function removeActiveClass(){
+    const buttons = document.querySelectorAll('.category-btn')
+    buttons.forEach(button => button.classList.remove('active'))
+    
+}
+const loadDetails = async ( video_id ) =>{
+  const response = await fetch(`https://openapi.programming-hero.com/api/phero-tube
+    /video/${video_id}`
+    );
+    const data = await response.json();
+}
+
+const displayDetails = (video) => {
+  
+
 }
 
 fetchVideo()
